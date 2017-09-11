0b8fe726a825d7c2b835567e882485ae32c7ca3a
Leo Y. Li <leo.yuxiu.li@gmail.com>
2017-09-11 10:17:29 -0600

    Version 0.0.1
    - Added additional npm packages
    - Restructuralized files & dirs
    - As a template

--- app.js
+++ app.js
@@ -1,46 +1,104 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+// ==============================
+//  APP INITIALIZATION
+// ==============================
+var express                 = require('express'),
+    expressSanitizer        = require('express-sanitizer'),
+    flash                   = require('connect-flash'),
+    path                    = require('path'),
+    logger                  = require('morgan'),
+    cookieParser            = require('cookie-parser'),
+    bodyParser              = require('body-parser'),
+    mongoose                = require("mongoose"),
+    methodOverride          = require('method-override');
+    //favicon                 = require('serve-favicon'),
+    //passport                = require('passport'),
+    //LocalStrategy           = require('passport-local'),
+    //passportLocalMongoose   = require('passport-local-mongoose');
 
 var app = express();
 
-// view engine setup
+
+
+// ==============================
+//  DB LINKAGE
+// ==============================
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB || 'mongodb://localhost/leoyli-main-dev', {useMongoClient: true});
+
+
+
+// ==============================
+//  VIEW ENGINE
+// ==============================
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
+// ==============================
+//  MIDDLEWARE
+// ==============================
+var middleware = require('./middleware');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSanitizer());
+app.use(methodOverride('_method'));
+app.use(flash());
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
+
+// ==============================
+//  PASSPORT
+// ==============================
+// app.use(require('express-session')({
+//     secret: 'to-be-defined',
+//     resave: false,
+//     saveUninitialized: false
+// }));
+//
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+
+
+// ==============================
+//  ROUTES
+// ==============================
+var index = require('./routes/index');
 
 app.use('/', index);
-app.use('/users', users);
 
+
+
+// ==============================
+//  ERRORS CONTROL
+// ==============================
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
+
+
+// ==============================
+//  APP EXPORTS
+// ==============================
 module.exports = app;
